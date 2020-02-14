7af32eceee0a3312b0f065b9e3bb1ff1e8b8709c
Manjunath <manjunathmorabad40@gmail.com>
2020-02-14 17:48:20 +0530

    initial commit

---
+++ src/resumeData.js
@@ -0,0 +1,106 @@
+let resumeData = {
+    "imagebaseurl":"https://github.com/morabad123",
+    "name": "Manju Morabad",
+    "role": "Student of KLE BCA",
+    "linkedinId":"Your LinkedIn Id",
+    "skypeid": "Your skypeid",
+    "roleDescription": "I like coding and simply play games in my free time.",
+    "socialLinks":[
+        {
+          "name":"github",
+          "url":"https://github.com/morabad123",
+          "className":"fa fa-gitub"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/morabad123",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"instagram",
+          "url":"http://instagram.com/manjunathmorabad",
+          "className":"fa fa-instagram"
+        }
+      ],
+    "aboutme":"I am currently a pre-final year student at kle BCA. I am a, I believe that to be successful in life,",
+    "address":"India",
+    "website":"https://github.com/morabad123",
+    "education":[
+      {
+        "UniversityName":"The KLE BCA HUBLI",
+        "specialization":"Some specialization",
+        "MonthOfPassing":"Aug",
+        "YearOfPassing":"2021",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "UniversityName":"Some University",
+        "specialization":"Some specialization",
+        "MonthOfPassing":"Jan",
+        "YearOfPassing":"2018",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
